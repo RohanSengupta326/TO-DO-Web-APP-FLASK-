3c52e56029c0c8cef4f0a3b75b21784d2d88f2fb
Rohan <rohansengupta326@gmail.com>
2021-06-20 13:30:47 +0530

    last

--- app.py
+++ app.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
-
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///todo.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
@@ -18,35 +17,20 @@ class Todo(db.Model):
         return ("{sno}-{title}".format(sno=self.sno, title=self.title))
 
 @app.route('/', methods=['GET', 'POST'])
-
 def hello_world():
     if request.method=='POST':
         title = request.form['title']
         desc = request.form['desc']
-
-        # how to add something to db below
         todo = Todo(title=title, desc=desc)
         db.session.add(todo)
         db.session.commit()
+        
+    allTodo = Todo.query.all() 
+    return render_template('index.html', allTodo=allTodo)
 
-    alltodo = Todo.query.all()
 
-    # returning html template and passing a python var to hmtl to show
-    # in on order to that i need to add jinja in html file
-    return render_template('index.html', alltodo=alltodo) 
-
-""" \home means different page, @app.route used to go to diff pages   """
-
-@app.route('/delete/<int:sno>')
-    
-def delete(sno):
-    todo = Todo.query.filter_by(sno=sno).first()
-    db.session.delete(todo)
-    db.session.commit()
-    return redirect("/")
 
 @app.route('/update/<int:sno>', methods=['GET', 'POST'])
-    
 def update(sno):
     if request.method=='POST':
         title = request.form['title']
@@ -57,10 +41,16 @@ def update(sno):
         db.session.add(todo)
         db.session.commit()
         return redirect("/")
-
+        
     todo = Todo.query.filter_by(sno=sno).first()
     return render_template('update.html', todo=todo)
 
-        
+@app.route('/delete/<int:sno>')
+def delete(sno):
+    todo = Todo.query.filter_by(sno=sno).first()
+    db.session.delete(todo)
+    db.session.commit()
+    return redirect("/")
+
 if __name__ == "__main__":
-    app.run(debug=True, port=33507)
+    app.run(debug=True, port=8000)
