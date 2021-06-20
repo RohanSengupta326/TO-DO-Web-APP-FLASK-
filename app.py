35b29ae6fa7daeb45087f5de9d12444a2e0e55bd
Rohan <rohansengupta326@gmail.com>
2021-06-20 13:02:42 +0530

    fifth

--- app.py
+++ app.py
@@ -63,5 +63,5 @@ def update(sno):
 
         
 if __name__ == "__main__":
-    PORT = int(os.environ.get('PORT', 8000))
+    PORT = int(os.environ.get('PORT', 33507))
     app.run(host='0.0.0.0', port=PORT, debug=True)
